5ebad8ff59972fb90507952e8dd19e860f6e38cb
techmannih <manishchaudharyttt@gmail.com>
2023-12-23 14:53:48 +0530

    server setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+
+const app = express()
+
+
+// variables
+const PORT = process.env.PORT || 8888
+
+
+// routes
+app.get('/', (req, res) => {
+    res.send("server running")
+})
+app.listen(PORT, function () {
+    console.log(`Listening on port ${PORT}`);
+  });
