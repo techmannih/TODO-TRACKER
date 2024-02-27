30fdd7d0899faa649e864cf369b4737d2ecd4161
techmannih <manishchaudharyttt@gmail.com>
2024-02-27 08:15:39 +0530

    mongo error

--- db/conn.js
+++ db/conn.js
@@ -1,16 +1,14 @@
-const mongoose =require("mongoose")
+const mongoose = require("mongoose");
 
-const connectDB =async () =>{
-    try{
-        const conn= await mongoose.connect(process.env.dbURL, {
-            useNewUrlParser: true
-        });
-        console.log("database connected successfully");
-    
-
-    } catch(err){
-        console.log(err.message);
-
-    }
-}
-module.exports = connectDB ;
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.dbURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("database connected successfully");
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+module.exports = connectDB;
